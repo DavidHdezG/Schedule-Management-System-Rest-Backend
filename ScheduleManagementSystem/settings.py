821cb679b460ec3525c1cfc92a482d90feb44a97
David Hernández <davidehdezgarcia@gmail.com>
2023-04-11 20:52:17 -0600

    Static Root fixed

--- ScheduleManagementSystem/settings.py
+++ ScheduleManagementSystem/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
